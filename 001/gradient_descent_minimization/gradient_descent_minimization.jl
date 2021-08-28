f10aade3ba41371e6ae917752fb077c4eb527acc
Colby Roberts <colbiverse@gmail.com>
2021-08-27 19:59:48 -0700

    Update gradient_descent_minimization.jl

--- 001/gradient_descent_minimization/gradient_descent_minimization.jl
+++ 001/gradient_descent_minimization/gradient_descent_minimization.jl
@@ -2,9 +2,9 @@ avg = []
 for i = 1:10
     start_time = time_ns()
 
-    derivatives = Dict('x' => (dx(coords) = 4 * coords['x']), 'y' => (dy(coords) = 6 * coords['y']))
-    best_coords = Dict('x' => 0, 'y' => 0) # f(x,y) = 1
-    current_coords = Dict('x' => 1, 'y' => 2) # f(x,y) = 15
+    derivatives = Dict('x' => (dx(coords) = 4.0 * coords['x']), 'y' => (dy(coords) = 6.0 * coords['y']))
+    best_coords = Dict('x' => 0.0, 'y' => 0.0) # f(x,y) = 1
+    current_coords = Dict('x' => 1.0, 'y' => 2.0) # f(x,y) = 15
 
     function descend(current_coords, learning_rate = 0.001)
         new_coords = Dict(cart => pos for (cart, pos) in current_coords)
@@ -17,7 +17,7 @@ for i = 1:10
     end
 
     function f(coords) 
-        return 1 + 2 * coords['x'] ^ 2 + 3 * coords['y'] ^ 2
+        return 1.0 + 2.0 * coords['x'] ^ 2.0 + 3.0 * coords['y'] ^ 2.0
     end
 
     while float(f(current_coords)) > float(f(best_coords)) # or for _ in range(10000)
@@ -27,4 +27,4 @@ for i = 1:10
     append!(avg, (time_ns() - start_time))
 end
 
-println("Julia: ", sum(avg) / 1000000000 / 10)
+println("Julia: ", sum(avg) / 1000000000.0 / 10.0)

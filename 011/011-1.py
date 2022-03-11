81532831c3811574a1f85e99933ef62c2b504035
Colby Roberts <colbiverse@gmail.com>
2022-03-11 22:53:35 +0000

    cringe

--- 011/011-1.py
+++ 011/011-1.py
@@ -1,41 +1,83 @@
-import random
+from numpy import random
+import math
 from tic_tac_toe import Game
 
-def generate_strategy(seed):
-    random.seed(seed)
-    strategy = {}
+def generate_strategy(num_of_branches = 3, depth = 0, max_depth = 9, strategy = {}, strings = [{'board':[0 for _ in range(9)], 'space': None}]): # depth is also turn number
 
-    for _ in range(25):
+    if depth == max_depth: return strategy
 
-        string = [0 for _ in range(9)]
+    player = (depth % 2) + 1
+    
+    new_strings = build_new_strings(strings, player, num_of_branches)
 
-        for i in range(9):
+    random.shuffle(new_strings)
+        
+    for string in new_strings:
+    
+        temp = string['board'][:string['space']] + [0] + string['board'][string['space'] + 1:]
+        state = ''.join([str(item) for item in temp])
 
-            player = (i % 2) + 1
-            choices = [i for i in range(9) if string[i] == 0]
-            space = random.choices(choices)[0]
-            strategy[''.join([str(item) for item in string])] = space
-            string[space] = player
+        if state in strategy.keys(): continue
+        
+        strategy[state] = string['space']
+
+    random.shuffle(new_strings)
+    return generate_strategy(num_of_branches, depth = depth + 1, strategy = strategy, strings = new_strings[:num_of_branches])
+
+    # strategy = {}
+    
+    # for _ in range(100):
+
+    #     string = [0 for _ in range(9)]
+
+    #     for i in range(9):
+
+    #         player = (i % 2) + 1
+    #         choices = [i for i in range(9) if string[i] == 0]
+    #         space = random.choice(choices)
+    #         strategy[''.join([str(item) for item in string])] = space
+    #         string[space] = player
             
-    return strategy
+    # return strategy
 
-def fight(i, strategy_one, strategy_two):
-    random.seed(i)
+def build_new_strings(strings, player, num_of_branches):
+
+    new_strings = []
+    
+    for string in strings:
+
+        for i in range(num_of_branches):
+                            
+            choices = [j for j in range(9) if string['board'][j] == 0]
+            space = random.choice(choices)
+            temp = string['board'][:space] + [player] + string['board'][space + 1:]
+            new_strings.append({'board': temp, 'space': space})
+
+    return new_strings
+
+def fight(i, strategy_one, strategy_two, count_score = True):
     game = Game(strategy_one['strategy'], strategy_two['strategy'])
     result = game.play()
     strategy_one['strategy'], strategy_two['strategy'] = result[1][0], result[1][1]
 
+    if not count_score: return strategy_one, strategy_two
+    
     if result[0][1] != 'Draw':
 
-        if result[0][1] == 1:
+        if result[0][1] == 1: # player 1
 
             strategy_one['score'] += 1
             strategy_two['score'] -= 1
 
-        elif result[0][1] == 2:
+        elif result[0][1] == 2: # player 2
 
             strategy_one['score'] -= 1
             strategy_two['score'] += 1
+        
+    # else: # draw
+
+    #     strategy_one['score'] += 1
+    #     strategy_two['score'] -= 1            
 
     return strategy_one, strategy_two
 
@@ -51,16 +93,31 @@ def breed(strategy_one, strategy_two):
 
         else:
 
-            baby_strategy['strategy'][key_1] = random.choices([value_1, value_2])[0]
+            baby_strategy['strategy'][key_1] = random.choice([value_1, value_2])
 
     return baby_strategy
 
+def calculate_score(best_five, strategies):
+
+    for i, strategy_one in enumerate(best_five[:5]):
+
+        for strategy_two in strategies[:5]:
+
+            best_five[i], _ = fight(i, strategy_one, strategy_two, False)
+
+    return sum([strategy['score'] / len(best_five) for strategy in best_five])
+
 strategies = []
-average_score = []
+original_average_score = []
+previous_average_score = []
 
 for i in range(25):
-    strategies.append({'strategy': generate_strategy(i), 'score': 0})
+    random.seed(i)
+    new_strategy = generate_strategy(num_of_branches=9)
+    strategies.append({'strategy': new_strategy, 'score': 0})
+    del new_strategy
 
+original_strategies = [strategy for strategy in strategies]
 previous_strategies = [strategy for strategy in strategies]
 
 for generation in range(1, 101):
@@ -74,38 +131,33 @@ for generation in range(1, 101):
             strategies[i], strategies[j] = fight(i, strategy_one, strategy_two)
 
     strategies.sort(key = lambda strategy: strategy['score'], reverse = True)
-    temp_1 = strategies[:5]
-    previous_strategies = [strategy for strategy in strategies]
-    del strategies
+    print("bad", strategies.count(strategies[-1]))
+    best_five = [strategy for strategy in strategies[:5]]
 
-    for strategy in temp_1: strategy['score'] = 0
+    for strategy in best_five: strategy['score'] = 0
 
-    for i, strategy_one in enumerate(temp_1):
+    previous_average_score.append(calculate_score(best_five, previous_strategies))
+    original_average_score.append(calculate_score(best_five, original_strategies))
+    new_strategies = [strategy for strategy in strategies]
 
-        for strategy_two in previous_strategies:
+    for i, strategy_one in enumerate(best_five):
 
-            temp_1[i], _ = fight(i, strategy_one, strategy_two)
-
-    average_score.append(sum([strategy['score'] / len(temp_1) for strategy in temp_1]) )
-    temp_2 = []
-
-    for strategy in temp_1: strategy['score'] = 0
-
-    for i, strategy_one in enumerate(temp_1):
-
-        for strategy_two in temp_1[:i] + temp_1[i + 1:]:
+        for strategy_two in best_five[:i] + best_five[i + 1:5]:
 
             new_strategy = breed(strategy_one, strategy_two)
-            temp_2.append(new_strategy)
+            new_strategies.append(new_strategy)
 
-    strategies = temp_2
-    
+    previous_strategies = [strategy for strategy in strategies]
+    strategies = best_five
+    del best_five
 
 import matplotlib.pyplot as plt
 
-plt.plot(list(range(100)), average_score)
+plt.plot(list(range(100)), previous_average_score)
+plt.plot(list(range(100)), original_average_score)
+plt.legend(['Original', 'Previous'])
 plt.savefig('images/011-1.png')
 
 print("\nTraining Complete")
 
-print("\nBest Strategy\n\n", strategies[0], "\n\n")
+print("\nBest Strategy\n\n", strategies[0]['strategy'], "\n\n")
--- 011/tic_tac_toe.py
+++ 011/tic_tac_toe.py
@@ -1,38 +1,38 @@
-import random
+from numpy import random
 
 class Game:
     def __init__(self, strategy_one, strategy_two):
         self.board = [0 for _ in range(9)]
 
         if isinstance(strategy_one, list): strategy_one = strategy_one[0]
+
         if isinstance(strategy_two, list): strategy_two = strategy_two[0]
+
         self.strategies = [strategy_one, strategy_two]
 
     def play(self):
         current_player = 0
-        new_possible_moves = [{}, {}]
+        #new_possible_moves = [{}, {}]
         while not self.game_finished()[0] and self.board.count(0) > 0:
             current_state = self.state()
             if current_state in self.strategies[current_player].keys():
                 current_move = self.strategies[current_player][current_state]
             else:
                 choices = [i for i in range(9) if self.board[i] == 0]
-                current_move = random.choices(choices)[0]
-                new_possible_moves[current_player][current_state] = current_move
+                current_move = random.choice(choices)
+                #new_possible_moves[current_player][current_state] = current_move
             self.place(current_player, current_move)
-            if current_player == 0: 
-                current_player = 1
-                continue
-            else:
-                current_player = 0
-                continue
+            
+            current_player = 1 if current_player == 0 else 0
 
         winner = self.game_finished()
 
-        for current_player, new_moves in enumerate(new_possible_moves):
-            if current_player + 1 == winner[1]:
-                for state, move in new_moves.items():
-                    self.strategies[current_player][state] = move
+        # new_possible_moves[current_player - 1][current_state] = current_move # next time dont let player win using same move
+
+        # for current_player, new_moves in enumerate(new_possible_moves):
+        #     if current_player + 1 == winner[1] or not winner[0]:
+        #         for state, move in new_moves.items():
+        #             self.strategies[current_player][state] = move
 
         if winner[1] == None and self.board.count(0) == 0:
             #print("\tDraw")
@@ -58,11 +58,16 @@ class Game:
         return ''.join([str(space) for space in self.board])
 
     def game_finished(self):
+        if self.board.count(0) > 4: return (False, None, None)
         for index, space in enumerate(self.board):
-            horizontal = index in [0,3,6] and self.board[index] != 0 and self.board[index + 1] == space and self.board[index + 2] == space
-            vertical = index in [0,1,2] and self.board[index] != 0 and self.board[index + 3] == space and self.board[index + 6] == space
-            forward_diagonal = self.board[index] != 0 and index == 0 and self.board[index + 4] == space and self.board[index + 8] == space
-            backward_diagonal = self.board[index] != 0 and index == 2 and self.board[index + 2] == space and self.board[index + 4] == space
-            if [horizontal, vertical, forward_diagonal, backward_diagonal].count(True) > 0:
-                return (True, space, [horizontal, vertical, forward_diagonal, backward_diagonal])
+
+            horizontal = index in [0, 3, 6] and not self.board[index] != 0 and self.board[index + 1] == space and self.board[index + 2] == space
+            vertical = index in [0, 1, 2] and not self.board[index] != 0 and self.board[index + 3] == space and self.board[index + 6] == space
+            forward_diagonal = index in [0, 4, 8] and not self.board[index] != 0 and self.board[0] == space and self.board[4] == space and self.board[8] == space
+            backward_diagonal = index in [2, 4, 6] and not self.board[index] != 0 and self.board[2] == space and self.board[4] == space and self.board[6] == space
+            
+            connected = [horizontal, vertical, forward_diagonal, backward_diagonal]
+
+            if connected.count(True) > 0: return (True, space, connected)
+
         return (False, None, None)
